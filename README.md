00c49575be67567aec446de6cdedde28fd9dc454
kenta5293 <kenta.03.1110@gmail.com>
2021-02-02 21:58:13 +0900

    Chapter #5.0 Clear!

---
+++ README.md
@@ -0,0 +1,9 @@
+# react-movie-web
+
+노마드코더 강의를 따라 만들어보는 React를 이용한 영화 웹서비스.
+
+(React Js Fundamentals Course (2019 Update!))
+: https://nomadcoders.co/react-fundamentals/lobby
+
+· 레이아웃은 메가박스 사이트를 참고했습니다.
+· Github 계정변경으로 현재 계정으로 이어서 올립니다.
---
+++ src/Movie.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Movie.css';
+
+function Movie({ id, year, title, summary, poster, genres }) {
+  return (
+    <div className="movies-list">
+      <img src={poster} alt={title} title={title} className="movie-poster" />
+      <div className="movie-data">
+        <h3 className="movie-title">{title}</h3>
+        <h5 className="movie-year">{year}</h5>
+        <ul className="movie-genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres-text">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie-summary">{summary}</p>
+        {/* <p className="movie-summary">{summary.slice(0, 180)}</p> : slice the Array*/}
+      </div>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
